e443d334546e22b5675bc73aa07444cd944d5e88
Guilherme-Mares <guilherme.ma@yahoo.com>
2025-08-20 14:07:24 -0300

    --9

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -6,23 +6,23 @@ public partial class App : Application
 {
     static SQLiteDatabaseHelper _db;
 
-    public static SQLiteDatabaseHelper Db 
+    public static SQLiteDatabaseHelper Db
     {
         get
         {
-            if(_db == null)
+            if (_db == null)
             {
                 string path = Path.Combine(
                         Environment.GetFolderPath(
                             Environment.SpecialFolder.LocalApplicationData),
                         "banco_sqlite_compras.db3");
-                    
+
                 _db = new SQLiteDatabaseHelper(path);
             }
 
             return _db;
         }
-    
+
     } 
     public App()
     {
--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -26,9 +26,7 @@ namespace MauiAppMinhasCompras.Helpers
         public Task<List<Produto>> Update(Produto p) 
         {
             string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
-            return _conn.QueryAsync<Produto>(
-                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
-            );
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
         }
 
         public Task<int> Delete(int id) 
